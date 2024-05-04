01c77ea0c3f2c29942b7610963ffd234da1b3788
Corey Schroeder <codingcorey365@gmail.com>
2024-05-04 13:37:34 -0500

    General API Call Class added

---
+++ APIsAndJSON/General API Call.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON;
+
+public class General_API_Call
+{
+    HttpClient client = new HttpClient();
+    public void MethodTest()
+    {
+        string filePath = File.ReadAllText("appsettings.json");
+        string testUrl = JObject.Parse(filePath).GetValue("TestUrl").ToString();
+        var dataFetched = client.GetStringAsync(testUrl).Result;
+        
+        Console.WriteLine(dataFetched);
+    }
+}
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
+            // create object
+            General_API_Call testing = new General_API_Call();
+            testing.MethodTest();
         }
     }
 }
